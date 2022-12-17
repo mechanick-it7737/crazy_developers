a0e62ac7820d4d03c06ea1dfecb17591a253a2a8
mechanick-it7737 <116840642+mechanick-it7737@users.noreply.github.com>
2022-12-17 14:37:58 +0000

    Deploying to gh-pages from @ mechanick-it7737/crazy_developers@a0b075502b389c83f02752002f4f9c06c1bd57c4 🚀

--- index.562330ae.js
+++
@@ -1,2 +0,0 @@
-$(".center").slick({centerMode:!0,centerPadding:"60px",slidesToShow:3,responsive:[{breakpoint:768,settings:{arrows:!1,centerMode:!0,centerPadding:"40px",slidesToShow:3}},{breakpoint:480,settings:{arrows:!1,centerMode:!0,centerPadding:"40px",slidesToShow:1}}]});
-//# sourceMappingURL=index.562330ae.js.map
