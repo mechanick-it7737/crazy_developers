fbff980939fba7e85f3541c15900aaadb4e8a46e
Rostyslav Statilko <116840642+mechanick-it7737@users.noreply.github.com>
2022-12-18 02:30:18 +0000

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,3 +1,17 @@
 $(document).ready(function () { 
-  $('.slider').slick();
+  $('.slider').slick({
+    arrows: true,
+    adaptiveHight: true,
+    slidesToShow:3,
+    slidesToScroll: 2,
+    spped: 1000,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnFocus:true, 
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: false,
+    variableWidth:false
+  });
 });
