bafee968bbe6480981d9cdc072fde24abc649f16
mechanick-it7737 <116840642+mechanick-it7737@users.noreply.github.com>
2022-12-19 00:37:23 +0000

    Deploying to gh-pages from @ mechanick-it7737/crazy_developers@96162674b86cef785a8c8a55ae92d95c0f1f9ef0 🚀

---
+++ index.5181d467.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!0,adaptiveHight:!0,slidesToShow:1,slidesToScroll:1,spped:1e3,infinite:!0,autoplay:!0,autoplaySpeed:1500,pauseOnFocus:!0,pauseOnHover:!0,pauseOnDotsHover:!0,draggable:!1,centerMode:!0})}));
+//# sourceMappingURL=index.5181d467.js.map
